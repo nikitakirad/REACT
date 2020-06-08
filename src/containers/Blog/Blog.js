eb7d38a3a1979300bde173f0d2e960e444226686
Nikita <2nov.nk@gmail.com>
2020-06-08 11:38:24 +0530

    changes done

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -23,13 +23,13 @@ class blog extends Component{
         if(this.props.isAuthenticated){
             this.setState({loading:true});
             const queryParams = '?auth=' + this.props.isAuthenticated + '&orderBy="userId"&equalTo="' + this.props.userid + '"';
-            // console.log(queryParams);
+            
             Axios.get('/posts.json' +queryParams)
             .then( response => {
                 const fetchedPostsFromServer = [];
-                // console.log(res.data);
+                
                 if(response.data === null){
-                    //alert('No post yet Please add it');
+                   
                     this.setState({loading:false});
                     
                 }
@@ -39,8 +39,7 @@ class blog extends Component{
                             ...response.data[key],
                             id: key
                         } );
-                        // console.log(fetchedOrders);
-                        // console.log(this.state.fetchedposts);
+                       
                     }
             }
                 this.setState({fetchedposts:fetchedPostsFromServer,loading:false});
@@ -52,13 +51,12 @@ class blog extends Component{
         let updatedPost={...this.state.post};
         updatedPost[event.target.name]=event.target.value;
         this.setState({post:updatedPost});
-        // console.log(this.state.post);
+        
     }
     savedDataToServerHandler=()=>{
         if(this.props.isAuthenticated){
             const postdata={...this.state.post,userId:this.props.userid};
-            // console.log(this.props.token);
-            //console.log(Validation(postdata));
+            
             console.log(postdata);
             let valid='';
             valid=Validation(postdata);
@@ -66,7 +64,7 @@ class blog extends Component{
                 console.log(valid);
                 Axios.post('/posts.json?auth=' + this.props.isAuthenticated,postdata)
                 .then( response => {
-                    // console.log( response.data );
+                    
                     alert("Successfully added...");
                     let data=[...this.state.fetchedposts];
                     
@@ -75,7 +73,7 @@ class blog extends Component{
                     this.setState({
                         fetchedposts:data})
                     ;
-                    // console.log(this.state.fetchedposts);
+                    
                 } )}
             else{
                 console.log(valid);
@@ -95,52 +93,11 @@ class blog extends Component{
                 for(let key in this.state.fetchedposts){
                     if(this.state.fetchedposts[key].id !== id){
                         updateFetchedPostAfterDelete.push(this.state.fetchedposts[key]);
-                        // console.log(updateFetchedPostAfterDelete);
                     }
                 }
-                // console.log(updateFetchedPostAfterDelete);
                 this.setState({fetchedposts:updateFetchedPostAfterDelete})
     })}}
-    // storeEditedPostOnServer={
-    //     title:'',
-    //     content:''
-    // }
-    // editHandler=(id,event)=>{
-    //     // console.log(event);
-    //     // console.log(id);
-    //     let editParticularPost=[...this.state.fetchedposts]
-    //     // console.log(this.storeEditedPostOnServer);
-    //     for(let key in editParticularPost){
-    //         if(editParticularPost[key].id === id ){
-    //             this.storeEditedPostOnServer={...editParticularPost[key]}
-    //             editParticularPost[key][event.target.name]=event.target.value;
-    //             this.storeEditedPostOnServer[event.target.name]=event.target.value;
-                
-    //         }
-    //     }
-    //     // console.log(editParticularPost);
-    //     let valid='';
-    //     valid=Validation(this.storeEditedPostOnServer);
-    //     if(valid){
-    //         this.setState({fetchedposts:editParticularPost});}
-    //     // console.log(this.storeEditedPostOnServer);
-    //     else{
-    //         alert('Please Fill out required fields');
-    //     }
-    // }
-    // updateValueToServer=(id)=>{
-    //     console.log(id);
-        
-        
-    //         Axios.patch(`posts/${id}.json`,this.storeEditedPostOnServer)
-    //         .then(response => {
-    //                 console.log(response)
-    //                 alert("Successfully edited...");
-    //         })
-    //         .catch(error => {
-    //             console.log(error)
-    //         })
-    // }
+    
     postSelectedHandler=(id)=>{
         this.props.history.push('/post/' +id);
     }
