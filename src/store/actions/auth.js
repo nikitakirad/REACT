5b12de56e42ddb988952fb169a4d91c8e082b896
Nikita <2nov.nk@gmail.com>
2020-06-08 11:23:39 +0530

    changes done

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,13 +22,7 @@ export const authFail = (error) => {
         error: error
     };
 };
-// export const checkAuthTimeout = (expirationTime) => {
-//     return dispatch => {
-//         setTimeout(() => {
-//             dispatch(logout());
-//         }, expirationTime * 1000);
-//     };
-// };
+
 export const logout = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('userId');
@@ -49,12 +43,9 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
-                // const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
                 localStorage.setItem('token', response.data.idToken);
-                // localStorage.setItem('expirationDate', expirationDate);
                 localStorage.setItem('userId', response.data.localId);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
-                // dispatch(checkAuthTimeout(response.data.expiresIn));
 
 
             })
@@ -63,22 +54,6 @@ export const auth = (email, password, isSignup) => {
             });
     };
 };
-// export const authCheckState = () => {
-//     return dispatch => {
-//         const token = localStorage.getItem('token');
-//         if (!token) {
-//             dispatch(logout());
-//         } else {
-//             const expirationDate = new Date(localStorage.getItem('expirationDate'));
-//             if (expirationDate <= new Date()) {
-//                 dispatch(logout());
-//             } else {
-//                 const userId = localStorage.getItem('userId');
-//                 dispatch(authSuccess(token, userId));
-//                 dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000 ));
-//             }   
-//         }
-//     };
-// };
+
 
 
